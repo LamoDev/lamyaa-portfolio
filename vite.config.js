16f079eed8d8b99aa416a42cf405cfda6cec0dd1
LamyaaHilal <lamyaahilal9@gmail.com>
2024-12-06 21:59:10 +0300

    config changes2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my_portfolio/' // Replace 'my_portfolio' with your GitHub repository name
+  base: '/my_portfolio' // Replace 'my_portfolio' with your GitHub repository name
 });
 
